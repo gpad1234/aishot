877098410f31ce981d143bead6f2a5f398e4b899
gp <gp@gps-MacBook.lan>
2024-02-27 13:29:41 -0500

    code explain

--- app.py
+++ app.py
@@ -18,12 +18,22 @@ def single_converter(menu):
 
 @app.get('/aihome')
 def single_aiconverter():
+  userText = request.args.get('msg')
+  messages_template=[
+        # {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "system", "content": "You will be provided with a piece of code, and your task is to explain it in a concise way"},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+      ]
+  messages_template[1] = {"role": "user", "content": userText }
+  print(messages_template)
+  #msgText={"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
   completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
-      messages=[
-         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-         {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-      ]
+      #messages=[
+         #{"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+         #{"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+      #]
+      messages=messages_template
   )
   answer=completion.choices[0].message
   print(answer)
